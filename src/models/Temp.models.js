d9ce97353dae3326e46fec14a67cd18ddd56bfe2
Foridul35962 <foridulislam35962@gmail.com>
2025-12-06 12:36:06 +0600

    temporary user model added

---
+++ src/models/Temp.models.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const tempSchema = new mongoose.Schema({
+    fullName:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    otp:{
+        type: String,
+        required: true
+    },
+    expiredOtp:{
+        type: Date,
+        required: true
+    },
+    isVerified:{
+        type: Boolean,
+        default: false
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now,
+        expires: 6000
+    }
+}, {timestamps: true})
+
+const Temp = mongoose.model('temp', tempSchema)
+
+export default Temp
